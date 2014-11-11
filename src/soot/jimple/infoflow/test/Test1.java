0f3600d628aee87957fb7f88c2a0395c9a532452
suresh <suresh301190@gmail.com>
2014-11-12 00:32:55 +0530

    Check-style working phase

--- src/soot/jimple/infoflow/test/Test1.java
+++ src/soot/jimple/infoflow/test/Test1.java
@@ -6,20 +6,34 @@ import java.io.InputStreamReader;
 
 public class Test1 {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		new Test1().work();
 	}
-	
-	public void work(){
-		try{
-			BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
-			int x = Integer.parseInt(new String(is.readLine()));
-			int y = x;
-			System.out.println(y);
-		}catch(IOException e){
-			e.printStackTrace();
-		}
+
+	public void work() throws IOException{
+		Runnable r = new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+				int x;
+				try {
+					x = Integer.parseInt(new String(is.readLine()));
+					int y = x;
+					System.out.println(y);
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}				
+		};
+		
+		new Thread(r).start();
 	}
 
 }
--- src/soot/jimple/infoflow/test/TestApp.java
+++ src/soot/jimple/infoflow/test/TestApp.java
@@ -13,17 +13,17 @@ public class TestApp {
 		
 		String[] args1 = new String[]{
 				//*
-				"D:\\BTSync\\Thesis\\test.jar",
-				"<Test1: void main(java.lang.String[])>"
+				"D:\\BTSync\\Thesis\\checkstyle-6.0\\checkstyle-6.0.jar",
+				"<com.puppycrawl.tools.checkstyle.Main: void main(java.lang.String[])>"
 				//*/
 				/*
-				"C:\\Users\\Aritra\\workspace\\Repair_TaintAnalysis\\Test1.jar",
-				"<soot.jimple.infoflow.test.Test1: void main(java.lang.String[])>"
+				"jperf.jar",
+				"<net.nlanr.jperf.JPerf: void main(java.lang.String[])>"
 				// */
 				};
 		SourceSinkResolver ssr = new SourceSinkResolver(args1, true);
 		//ssr.setAccessPathLength(3);
 		ssr.runAnalysis();
-		System.out.println(ssr.toStringMethodToChainMap());
+		//System.out.println(ssr.toStringMethodToChainMap());
 	}
 }
